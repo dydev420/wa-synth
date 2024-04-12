1c3066657048871b84040bb8efb8f8502b0f4af6
DY Dev <dydev420@gmail.com>
2024-04-12 23:16:25 +0530

    feat: added manifest.json with default vite icons and install unlocked

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './service-worker.js',
+          dest: './'
+        }
+      ]
+    }),
+  ],
 })
