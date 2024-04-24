fb3459a871d6c0743037f94db38b577e32100c07
DY Dev <dydev420@gmail.com>
2024-04-25 04:09:07 +0530

    feature: added initial pwa icons and service worker caching

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        // the default entry point
+        app: './index.html',
+
+        // Separate entry for service worker
+        'service-worker': './service-worker.js',
+      },
+      output: {
+        // without hash file names
+        entryFileNames: assetInfo => {
+          return assetInfo.name === 'service-worker'
+             ? '[name].js'         // put service worker in root
+             : 'assets/[name].js' // temp without hash
+        },
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
+  },
   plugins: [
     react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './service-worker.js',
-          dest: './'
-        }
-      ]
-    }),
   ],
 })
